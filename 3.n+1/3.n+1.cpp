53c34d6d12ec69569ae675a9099974930d8a6565
Daniel Saguez Tezanos Pinto <daniel@saguez.org>
2016-04-08 15:52:21 -0400

    Solution with boost fast_pool_alloc

--- 3.n+1/3.n+1.cpp
+++ 3.n+1/3.n+1.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <cmath>
 #include <map>
+#include <boost/pool/pool_alloc.hpp>
 
 int maxCiclos(int, int);
 int contCiclos(long);
@@ -11,6 +12,7 @@ int main(int argc, char *argv[]){
     int i = atoi(argv[1]);
     int j = atoi(argv[2]);
     std::cout<<i<<' '<<j<<' '<< maxCiclos(i,j)<< std::endl;
+    boost::singleton_pool<boost::fast_pool_allocator_tag, sizeof(std::pair<const long, int>)>::release_memory();
 }
 
 int maxCiclos(int i, int j){
@@ -25,18 +27,36 @@ int maxCiclos(int i, int j){
 
 int contCiclos(long i){
     //std::cout<<" i="<<i<<"; ";
+    static std::map<long,int,std::less< long >, boost::pool_allocator<std::pair<const long,int> > > cache;
+    std::vector<int  > lista;
+    int conCache = 0;
     int c = 0;
     while( !( i == (i & -i) ) ){
+        if(cache.count(i)){
+            //std::cout<<"en cache "<< cache[i]<< "; c="<<c;
+            conCache=cache[i];
+            break;
+        } else {
+            lista.push_back(i);
+            //std::cout<<" i="<<i;
+        }
         if(i&1) {
-            i = (3*i+1)>>1;
-            c+=2;
+            i = (3*i+1);
+            c++;
         } else {
             i>>=1;
             c++;
         }
     }
-    if(i != 1) {
-        c += std::log2(i)+1;
+    if(conCache){
+        c += conCache;
+    } else {
+        if(i != 1) {
+            c += std::log2(i)+1;
+        }
+    }
+    for(int d = 0; d < lista.size();d++){
+        cache[lista[d]] = c-d;
     }
     //std::cout<<"cont="<<c<<std::endl;
     return c;
